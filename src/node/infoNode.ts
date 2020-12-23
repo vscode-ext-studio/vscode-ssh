eafc424b73d33bdfc47b2df345e14b61440f543e
cweijan <cweijan@163.com>
2020-12-23 10:59:54 +0800

    Add link node icon

--- src/node/infoNode.ts
+++ src/node/infoNode.ts
@@ -1,6 +1,8 @@
 import { setupMaster } from "cluster";
-import { TreeItemCollapsibleState } from "vscode";
+import { TreeItemCollapsibleState, Uri } from "vscode";
+import { version } from "vue/types/umd";
 import { NodeType } from "../common/constant";
+import { Util } from "../common/util";
 import AbstractNode from "./abstracNode";
 
 export class InfoNode extends AbstractNode {
@@ -18,7 +20,10 @@ export class LinkNode extends AbstractNode {
     contextValue = NodeType.Link;
     constructor(info: string) {
         super(info)
-        // this.iconPath=
+        this.iconPath={
+            light: Util.getExtPath("resources", "image", "light", "link.svg"),
+            dark: Util.getExtPath("resources", "image","dark", "link.svg"),
+        }
         this.collapsibleState = TreeItemCollapsibleState.None
     }
     getChildren(): Promise<AbstractNode[]> {
